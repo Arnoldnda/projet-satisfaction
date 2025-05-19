a359de821da2f52663eb0e6fa97b8a2e8ce4489b
arnold Nda <ndadesirarnold@icloud.com>
2025-05-19 01:55:11 +0000

    coonexion de l'application à la base de donnée

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 const express = require('express') 
 const morgan = require('morgan')
+const {Sequelize} = require('sequelize')
 
 const app = express()
 const port = 3000 
 
-app.use(morgan('dev'))
+const sequelize = new Sequelize('project_satisfation', 'root', '', {
+    host: 'localhost',
+    dialect: 'mariadb',
+    dialectOptions: {
+        timezone: 'Etc/GMT-2',
+    },
+    logging: false
+})
+
+sequelize.authenticate()
+.then(_ => console.log('La connexion à la base de donnée à bien été établie'))
+.catch(error => console.log(`Impossible de se connecté a la base de donnée ${error}`)) 
+
+app
+.use(morgan('dev'))
+.use(express.json())
 
 app.get('/', (req, res) => res.send('Hello world.'))
 
