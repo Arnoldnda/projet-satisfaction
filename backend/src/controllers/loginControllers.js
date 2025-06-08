4b7fa59140586fccc43d079c6438d96c021eecd2
arnold Nda <ndadesirarnold@icloud.com>
2025-06-08 12:44:54 +0000

    gerer les erreurs

--- backend/src/controllers/loginControllers.js
+++ backend/src/controllers/loginControllers.js
@@ -3,23 +3,30 @@ const bcrypt = require('bcrypt')
 
 exports.loginAdmin = async (req, res) => {
 
-    const { email, passwd } = req.body 
-    
-    const Admin = await Administrateur.findOne({ where: { email } })
+    try {
 
-    if (!Admin) {
-        return res.status(404).json({ message: "Administrateur non trouvé." });
-    }
-
-    const isPasswordValid = await bcrypt.compare(passwd, Admin.passwd)
-
-    if (isPasswordValid) {
-
-        const message = `L'utilisateur à été connecté avec succès`
-        return res.status(200).json({message, data: Admin})
-
-    } else {
+        const { email, passwd } = req.body 
         
-        return res.status(401).json({ message: "Mot de passe incorrect." });
+        const Admin = await Administrateur.findOne({ where: { email } })
+
+        if (!Admin) {
+            return res.status(404).json({ message: "Administrateur non trouvé." });
+        }
+
+        const isPasswordValid = await bcrypt.compare(passwd, Admin.passwd)
+
+        if (isPasswordValid) {
+
+            const message = `L'utilisateur à été connecté avec succès`
+            return res.status(200).json({message, data: Admin})
+
+        } else {
+            
+            return res.status(401).json({ message: "Mot de passe incorrect." });
+        }
+
+    }catch(error){
+        const message = `L'utilisateur n'a pas pu être connecté. Réessayez dans quelque instants.`
+        return res.status(500).json({message , data: error})
     }
 }
